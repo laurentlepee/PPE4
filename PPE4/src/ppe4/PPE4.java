dc9ca84e2d1050cadd798e77b9c452c0e1a5937e
btssio <btssio@localhost.localdomain>
2012-12-03 14:51:11 +0100

    commit de base

---
+++ PPE4/src/ppe4/PPE4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppe4;
+
+/**
+ *
+ * @author btssio
+ */
+public class PPE4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
