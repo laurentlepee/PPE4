d051f7804b9005c7baac6629f89249244bae4f8a
btssio <btssio@localhost.localdomain>
2012-12-03 15:22:50 +0100

    MVC

---
+++ PPE4/src/ppe4/Modele/Modele.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppe4.Modele;
+
+/**
+ *
+ * @author btssio
+ */
+public class Modele {
+    
+}
