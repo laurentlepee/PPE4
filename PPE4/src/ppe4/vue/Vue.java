c35088a3dc72265c5d8f2b186c7b00830adbac24
Drouaud Aurelien <a.drouaud@gmail.com>
2012-12-10 14:52:44 +0100

    Test

---
+++ PPE4/src/ppe4/vue/Vue.java
@@ -0,0 +1,13 @@
+/* test
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppe4.vue;
+
+/**
+ *
+ * @author adrouaud
+ */
+public class Vue {
+    
+}
