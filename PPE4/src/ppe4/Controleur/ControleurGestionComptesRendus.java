2c0ef4e5ec8e09b31bf7d53771a85ba87125697c
Loïc Désirest <ldesirest@gmail.com>
2012-12-17 16:10:06 +0100

    Certains noms de variables ont été corrigés (reste DAO) Le premier controleur (ControleurGestionComptesRendus) a été créer et il fonctionne !

---
+++ PPE4/src/ppe4/Controleur/ControleurGestionComptesRendus.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppe4.Controleur;
+import ppe4.Vue.RapportVisite;
+
+/**
+ *
+ * @author ldesirest
+ */
+public class ControleurGestionComptesRendus {
+   
+    //On créer une méthode run pour le controleur pour pouvoir éxécuter du code
+    
+    public void run(){
+        
+    // On instancie une nouvelle fenetre et on l'affiche
+    RapportVisite unRapport = new RapportVisite();
+    unRapport.setVisible(true);
+    }
+}
---
+++ PPE4/src/ppe4/Controleur/NewMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppe4.Controleur;
+
+/**
+ *
+ * @author ldesirest
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        
+    ControleurGestionComptesRendus unControleur = new ControleurGestionComptesRendus();
+    unControleur.run();
+        
+    }
+}
