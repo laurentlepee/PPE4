2beb3cc99308465c700f70a2161d44eed36b344b
btssio <btssio@localhost.localdomain>
2012-12-17 14:54:58 +0100

    Modele oracle

---
+++ PPE4/src/ppe4/Modele/ModeleOracle.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppe4.Modele;
+/**
+ *
+ * @author btssio
+ */
+public ModeleOracle(String , String loginBd, String mdpBd) {
+        super("oracle.jdbc.driver.OracleDriver", "jdbc:oracle:thin:localhost/base_de_données@//localhost:1521/xe", loginBd, mdpBd);
+    }
+}
