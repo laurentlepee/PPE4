e7c99c4b940a03d9c9b4f5299f105ef5c8e433e1
btssio <btssio@localhost.localdomain>
2013-01-08 11:11:02 +0100

    Dao Vis

--- PPE4/src/modele/dao/Dao.java
+++ PPE4/src/modele/dao/Dao.java
@@ -1,6 +1,9 @@
 package modele.dao;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import modele.metier.Visiteur;
 /**
  *
  * @author btssio
@@ -16,7 +19,7 @@ public abstract class Dao {
     // requêtes à préparer
     private PreparedStatement pstmtPraticien;
     private PreparedStatement pstmtPraticienSelection;
-    private PreparedStatement pstmtVisiteur;
+    private PreparedStatement pstmtUnVisiteur;
     private PreparedStatement pstmtVisiteurSelection;
     
 
@@ -35,8 +38,8 @@ public abstract class Dao {
                     "SELECT PRA_NOM, PRA_PRENOM FROM PRATICIEN", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             pstmtPraticienSelection = cnx.prepareStatement(
                     "SELECT * FROM PRATICIEN WHERE PRA_NUM=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            pstmtVisiteur = cnx.prepareStatement(
-                    "SELECT Vis_NOM, VIS_PRENOM FROM VISITEUR", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            pstmtUnVisiteur = cnx.prepareStatement(
+                    "SELECT VIS_MATRICULE, VIS_NOM, VIS_PRENOM FROM VISITEUR", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             pstmtVisiteurSelection = cnx.prepareStatement(
                     "SELECT * FROM VISITEUR WHERE PRA_NUM=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         } catch (SQLException ex) {
@@ -45,7 +48,33 @@ public abstract class Dao {
             throw new DaoException("DAO - connecter : pb de pilote JDBC\n" + ex.getMessage());
         }
     }
-
+    
+    public Visiteur chargerUnEnregistrementVisiteur(ResultSet rs) throws DaoException{
+         try {
+            Visiteur visiteur = new Visiteur();
+            visiteur.setVIS_MATRICULE(rs.getString("VIS_MATRICULE"));
+            visiteur.setVIS_NOM(rs.getString("VIS_NOM"));
+            visiteur.setVIS_PRENOM(rs.getString("VIS_PRENOM"));
+            return visiteur;
+        } catch (SQLException ex) {
+            throw new DaoException("DAO - chargerUnEnregistrementVisiteur : pb JDBC\n" + ex.getMessage());
+        }
+    }
+    
+    public Visiteur lireUnVisiteur(int id) throws DaoException {
+        try {
+            Visiteur visiteur = null;
+            pstmtUnVisiteur.setInt(1, id);
+            ResultSet rs = pstmtUnVisiteur.executeQuery();
+            if (rs.next()) {
+                visiteur = chargerUnEnregistrementVisiteur(rs);
+            }
+            return visiteur;
+        } catch (SQLException ex) {
+            throw new DaoException("DAO - lireUnVisiteur : pb JDBC\n" + ex.getMessage());
+        }
+    }
+    
     public void deconnecter() throws DaoException {
         try {
             cnx.close();
